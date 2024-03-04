0d818fc87a47f09f6495621b9c64457791c9da06
Braden Everson <bradeneverson@gmail.com>
2024-03-04 08:00:19 -0600

    64

--- src/core/complex.rs
+++ src/core/complex.rs
@@ -140,3 +140,142 @@ impl From<(i16, i16)> for c32 {
     } 
 }
 
+#[allow(non_camel_case_types)]
+//I want it to look like the traditional prim types
+#[derive(Debug)]
+pub struct c64 {
+    real: i32,
+    imaginary: i32
+}
+
+impl std::ops::Add<c64> for c64 {
+    type Output = c64;
+    fn add(self, rhs: c64) -> Self::Output {
+        c64 {
+            real: self.real + rhs.real,
+            imaginary: self.imaginary + rhs.imaginary
+        }
+    }
+}
+
+impl std::ops::Sub<c64> for c64 {
+    type Output = c64;
+    fn sub(self, rhs: c64) -> Self::Output {
+        c64 {
+            real: self.real - rhs.real,
+            imaginary: self.imaginary - rhs.imaginary
+        }
+    }
+}
+
+impl std::ops::Mul<c64> for c64 {
+    type Output = c64;
+    fn mul(self, rhs: c64) -> Self::Output {
+        c64 {
+            real: self.real * rhs.real,
+            imaginary: self.imaginary * rhs.imaginary
+        }
+    }
+}
+
+impl std::ops::Div<c64> for c64 {
+    type Output = c64;
+    fn div(self, rhs: c64) -> Self::Output {
+        c64 {
+            real: self.real / rhs.real,
+            imaginary: self.imaginary / rhs.imaginary
+        }
+    }
+}
+
+impl std::ops::Rem<c64> for c64 {
+    type Output = c64;
+    fn rem(self, rhs: c64) -> Self::Output {
+        c64 {
+            real: self.real % rhs.real,
+            imaginary: self.imaginary % rhs.imaginary
+        }
+    }
+}
+
+impl std::ops::AddAssign<c64> for c64 {
+    fn add_assign(&mut self, rhs: c64) {
+        self.real += rhs.real;
+        self.imaginary += rhs.imaginary;
+    }
+}
+
+
+impl std::ops::SubAssign<c64> for c64 {
+    fn sub_assign(&mut self, rhs: c64) {
+        self.real -= rhs.real;
+        self.imaginary -= rhs.imaginary;
+    }
+}
+
+impl std::ops::MulAssign<c64> for c64 {
+    fn mul_assign(&mut self, rhs: c64) {
+        self.real *= rhs.real;
+        self.imaginary *= rhs.imaginary;
+    }
+}
+
+impl std::ops::DivAssign<c64> for c64 {
+    fn div_assign(&mut self, rhs: c64) {
+        self.real /= rhs.real;
+        self.imaginary /= rhs.imaginary;
+    }
+}
+
+impl std::ops::RemAssign<c64> for c64 {
+    fn rem_assign(&mut self, rhs: c64) {
+        self.real %= rhs.real;
+        self.imaginary %= rhs.imaginary;
+    }
+}
+impl c64 {
+    pub fn new(real: i32, imaginary: i32) -> c64 {
+        c64 { real, imaginary }
+    }
+    pub fn modulus(&self) -> f64 {
+        (self.real.pow(2) as f64 + self.imaginary.pow(2) as f64).sqrt()
+    }
+    
+}
+
+impl PartialEq for c64 {
+    fn eq(&self, other: &Self) -> bool {
+        self.real == other.real && self.imaginary == other.imaginary
+    }
+}
+
+impl PartialOrd for c64 {
+    fn ge(&self, other: &Self) -> bool {
+        self.modulus() >= other.modulus()
+    }
+    fn gt(&self, other: &Self) -> bool {
+        self.modulus() > other.modulus()
+    }
+    fn lt(&self, other: &Self) -> bool {
+        self.modulus() < other.modulus()
+    }
+    fn le(&self, other: &Self) -> bool {
+        self.modulus() <= other.modulus()
+    }
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+            if self == other {
+                return Some(Ordering::Equal);
+            } else if self.modulus() > other.modulus() {
+                return Some(Ordering::Greater);
+            } else if self.modulus() < other.modulus() {
+                return Some(Ordering::Less);
+            }
+            None
+    }
+}
+
+impl From<(i32, i32)> for c64 {
+    fn from(value: (i32, i32)) -> Self {
+        c64::new(value.0, value.1)
+    } 
+}
--- src/core/test.rs
+++ src/core/test.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod test {
-    use crate::core::complex::c32;
+    use crate::core::complex::{c32, c64};
 
     #[test]
     fn test_mul() {
@@ -31,6 +31,38 @@ mod test {
     fn test_tuple() {
         let a: c32 = (1024,3).into();
         let b: c32 = c32::new(1024, 3);
+
+        assert!(a == b)
+    }
+    #[test]
+    fn test_mul_64() {
+        let a: c64 = c64::new(50, 3);
+        let b: c64 = (3, 5).into();
+
+        let c = a * b;
+        assert_eq!(c, (150,15).into())
+    }
+
+    #[test]
+    fn test_ge_64() {
+        let a: c64 = (10,2).into();
+        let b: c64 = (3, 5).into();
+
+        assert!(a > b)
+    }
+
+    #[test]
+    fn test_lt_64() {
+        let a: c64 = (10,2).into();
+        let b: c64 = (3, 5).into();
+
+        assert!(b < a)
+    }
+
+    #[test]
+    fn test_tuple_64() {
+        let a: c64 = (1024,3).into();
+        let b: c64 = c64::new(1024, 3);
 
         assert!(a == b)
     }
